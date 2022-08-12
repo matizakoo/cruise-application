3d36e54cbfaeee01138b16a47b6f328bad58ab0a
Mateusz Żak <matzakoo@gmail.com>
2022-08-12 08:09:49 +0200

    sorting by date/cost

---
+++ src/main/java/com/zak/cruise/controller/MyErrorController.java
@@ -0,0 +1,14 @@
+package com.zak.cruise.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyErrorController implements ErrorController {
+    @GetMapping("/error")
+    public String handleError() {
+        return "login";
+    }
+}
